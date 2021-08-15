683f7a4278c4833ef4ee1c9cf1b5fc3072029de9
Yaman KATBY <m.yaman.katby@gmail.com>
2021-08-15 17:14:48 +0300

    [docs] Add Layout files and config absolute paths

---
+++ docs/components/layout/Layout.js
@@ -0,0 +1,5 @@
+const Layout = () => {
+	return <h1>Hi, Layout component!</h1>;
+};
+
+export default Layout;
--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,5 +1,9 @@
+import { Layout } from "@components/layout";
+
 export default function index() {
   return (
+    <Layout>
       <h1>Hi, index page!</h1>
+    </Layout>
   );
 }
