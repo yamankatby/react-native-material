c40fb0c6c99605c1730151a01162a83f58455b3c
Yaman KATBY <m.yaman.katby@gmail.com>
2021-08-15 17:59:53 +0300

    [docs] Add Material UI package and create the Layout

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,5 +1,22 @@
+import { createTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#212121',
+        },
+        secondary: {
+            main: '#000',
+        },
+    },
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
