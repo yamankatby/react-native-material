5cef7a56aa3bf736f0c35d1ee8f05dae3df551be
Yaman KATBY <m.yaman.katby@gmail.com>
2021-08-23 12:56:57 +0300

    [example] Add SurfaceExample screen

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,39 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  Surface,
+  Typography,
+  ThemeProvider,
+  createTheme,
+} from "@react-native-material/lab";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { View, Button } from "react-native";
+import SurfaceExample from "./screens/SurfaceExample";
 
 const Stack = createNativeStackNavigator();
 
 const Home = () => {
-  return null;
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Button
+        title="Surface Example"
+        onPress={() => navigation.navigate("SurfaceExample")}
+      />
+    </View>
+  );
 };
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SurfaceExample" component={SurfaceExample} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
---
+++ example/screens/SurfaceExample.tsx
@@ -0,0 +1,51 @@
+import {
+  Elevation,
+  Surface,
+  Typography,
+  useTheme,
+} from "@react-native-material/lab";
+import React from "react";
+import { FlatList, View } from "react-native";
+
+const data: Elevation[] = [0, 1, 2, 3, 4, 6, 8, 12, 16, 24];
+
+const SurfaceExample: React.FC = () => {
+  const theme = useTheme();
+
+  return (
+    <FlatList
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flex: 1 }}
+      data={data}
+      numColumns={2}
+      renderItem={({ item }) => (
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            height: 160,
+          }}
+        >
+          <Surface
+            surfaceSize={{ width: 120, height: 120 }}
+            elevation={item}
+            containerStyle={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            overlayColor={theme.palette.surface.on}
+            backgroundColor={theme.palette.elevationColors[item]}
+          >
+            <Typography style={{ color: theme.palette.surface.on }}>
+              {item}
+            </Typography>
+          </Surface>
+        </View>
+      )}
+      keyExtractor={(item) => `${item}`}
+    />
+  );
+};
+
+export default SurfaceExample;
