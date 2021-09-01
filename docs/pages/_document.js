f211f9324b3d28c419afcc6e5179afbe51631bfd
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-01 13:24:30 +0300

    [docs] Add GTAG script

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -14,6 +14,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
             rel="stylesheet"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-5L00H23VKJ"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-5L00H23VKJ');
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
