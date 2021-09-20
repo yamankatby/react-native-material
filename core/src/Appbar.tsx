fd5743b2679c1d93da46fe112d2a64c8369f0f66
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-20 14:43:34 +0300

    [core] Work on the Appbar component

--- core/src/Appbar.tsx
+++ core/src/Appbar.tsx
@@ -1,11 +1,21 @@
-import React, { useMemo } from "react";
-import { Platform, StyleProp, TextStyle, useWindowDimensions, View, ViewStyle } from "react-native";
-import { ColorName, useStyleSheet } from "./base";
+import React, { useEffect, useMemo, useRef } from "react";
+import {
+  Animated,
+  Platform,
+  StyleProp,
+  StyleSheet,
+  TextStyle,
+  useWindowDimensions,
+  View,
+  ViewStyle,
+} from "react-native";
+import { ColorName, useStyleSheet, useTheme } from "./base";
 import Surface from "./Surface";
-import Typography from "./Typography";
 
 export interface AppbarProps {
   title?: string | React.ReactElement | undefined;
+
+  variant?: 'regular' | 'prominent' | undefined;
 
   leading?: React.ReactElement | undefined;
 
@@ -32,6 +42,7 @@ export interface AppbarProps {
 
 const Appbar: React.FC<AppbarProps> = ({
   title,
+  variant,
   leading,
   trailing,
   color,
@@ -48,56 +59,97 @@ const Appbar: React.FC<AppbarProps> = ({
   const defaultSize = useMemo(() => dimensions.width < 600, [dimensions.width]);
   const hasLeading = useMemo(() => !!leading, [leading])
 
-  const styles = useStyleSheet(({ colorScheme }) => ({
+  const animated = useRef(new Animated.Value(variant === 'regular' ? 0 : 1)).current
+
+  const regularHeight = Platform.OS === 'ios' ? 44 : defaultSize ? 56 : 64
+  const prominentHeight = 128
+
+  const theme = useTheme()
+
+  useEffect(() => {
+    Animated.timing(animated, {
+      toValue: variant === 'regular' ? 0 : 1,
+      duration: 200,
+      useNativeDriver: false,
+    }).start()
+  }, [variant])
+
+  const height = animated.interpolate({
+    inputRange: [0, 1],
+    outputRange: [regularHeight, prominentHeight],
+  })
+
+  const scale = animated.interpolate({
+    inputRange: [0, 1],
+    outputRange: [theme.typographyScheme.h6.fontSize!, theme.typographyScheme.h5.fontSize!],
+  })
+
+  const bottom = animated.interpolate({
+    inputRange: [0, 1],
+    outputRange: [(regularHeight - 24) / 2, 28],
+  })
+
+  const opacity = animated.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+  })
+
+  const styles = useStyleSheet(({ colorScheme, elevationScheme, typographyScheme }) => ({
     container: {
       backgroundColor: colorScheme[color!],
-      borderRadius: 0,
       elevation: 4,
+      borderRadius: 0,
     },
-    innerContainer: {
+    innerContainer: {},
+    toolbar: {
       flexDirection: "row",
       justifyContent: hasLeading ? 'space-between' : 'flex-end',
-      alignItems: "center",
-      height: Platform.select({ ios: 44, default: defaultSize ? 56 : 64 }),
-      marginHorizontal: defaultSize ? 4 : 12,
+      alignItems: 'center',
+      paddingHorizontal: defaultSize ? 4 : 12,
+      height: regularHeight,
     },
     titleContainer: {
       position: 'absolute',
-      start: 12,
-      end: 12,
-      justifyContent: "center",
+      start: 16,
+      end: 16,
+      justifyContent: 'center',
       height: 24,
-      paddingHorizontal: hasLeading ? 52 : 0,
+      paddingHorizontal: hasLeading ? 56 : 0,
     },
     title: {
+      ...typographyScheme.h6,
+      color: 'white',
       textAlign: centerTitle === true ? 'center' : undefined,
     },
     trailingContainer: {
       flexDirection: "row",
       justifyContent: "flex-end",
     },
-  }), [color, defaultSize, hasLeading, centerTitle]);
+  }), [color, defaultSize, hasLeading, regularHeight, centerTitle]);
 
   return (
     <Surface style={[styles.container, style]}>
-      <View style={[styles.innerContainer, innerContainerStyle]}>
-        {typeof title === "string" ? (
-          <View style={[styles.titleContainer, titleContainerStyle]}>
-            <Typography variant="h6" color={tintColor} style={[styles.title, titleStyle]}>
-              {title}
-            </Typography>
-          </View>
-        ) : (
-          title
-        )}
-        {leading && <View style={leadingContainerStyle}>{leading}</View>}
-        {trailing && <View style={[styles.trailingContainer, trailingContainerStyle]}>{trailing}</View>}
-      </View>
+      <Animated.Image
+        style={[StyleSheet.absoluteFillObject, { opacity }]}
+        source={{
+          uri: 'https://i.pinimg.com/originals/42/f2/14/42f214e4d180133b810b1d2b252cf389.png',
+        }}
+      />
+      <Animated.View style={[styles.innerContainer, { height }]}>
+        <Animated.View style={[styles.titleContainer, { bottom }]}>
+          <Animated.Text style={[styles.title, { fontSize: scale }]}>{title}</Animated.Text>
+        </Animated.View>
+        <View style={[styles.toolbar]}>
+          {leading && <View style={leadingContainerStyle}>{leading}</View>}
+          {trailing && <View style={[styles.trailingContainer, trailingContainerStyle]}>{trailing}</View>}
+        </View>
+      </Animated.View>
     </Surface>
   );
 };
 
 Appbar.defaultProps = {
+  variant: 'regular',
   color: "primary",
   tintColor: "onPrimary",
   centerTitle: Platform.OS === 'ios',
--- example/screens/Main.tsx
+++ example/screens/Main.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Platform, View } from "react-native";
+import React, { useState } from "react";
+import { Platform, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Appbar, IconButton, useTheme } from "@react-native-material/core";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
@@ -10,47 +10,81 @@ const Main = () => {
   const insets = useSafeAreaInsets();
   const { colorScheme } = useTheme();
 
+  const [x, setX] = useState(false)
+
   return (
     <View style={{ flex: 1, backgroundColor: colorScheme.background }}>
       <Appbar
         title="Material UI"
-        centerTitle={false}
+        variant={x ? 'prominent' : 'regular'}
+        // centerTitle={false}
+        tintColor={'onBackground'}
         leading={
           <IconButton
-            color={'onPrimary'}
-            icon={<MaterialCommunityIcons name={"menu"} size={24} color={colorScheme.onPrimary} />}
-            onPress={() => undefined}
+            color={'onBackground'}
+            icon={<MaterialCommunityIcons name={"menu"} size={24} color={colorScheme.onBackground} />}
+            onPress={() => setX(p => !p)}
           />
         }
         trailing={[
-          <IconButton
-            key={0}
-            color={'onPrimary'}
-            icon={<MaterialCommunityIcons name={"heart"} size={24} color={colorScheme.onPrimary} />}
-            onPress={() => undefined}
-          />,
+          // <IconButton
+          //   key={0}
+          //   color={'onPrimary'}
+          //   icon={<MaterialCommunityIcons name={"heart"} size={24} color={colorScheme.onPrimary} />}
+          //   onPress={() => setX(p => !p)}
+          // />,
           <IconButton
             key={1}
-            color={'onPrimary'}
-            icon={<MaterialCommunityIcons name={"magnify"} size={24} color={colorScheme.onPrimary} />}
+            color={'onBackground'}
+            icon={<MaterialCommunityIcons name={"magnify"} size={24} color={colorScheme.onBackground} />}
             onPress={() => undefined}
           />,
           <IconButton
             key={2}
-            color={'onPrimary'}
+            color={'onBackground'}
             icon={
               <MaterialCommunityIcons
                 name={Platform.OS === "ios" ? "dots-horizontal" : "dots-vertical"}
                 size={24}
-                color={colorScheme.onPrimary}
+                color={colorScheme.onBackground}
               />
             }
             onPress={() => undefined}
           />,
         ]}
-        style={{ paddingTop: insets.top }}
+        style={{ paddingTop: insets.top, elevation: 0, backgroundColor: chroma('white').alpha(0.9).hex(), position: 'absolute', top: 0, start: 0, end: 0, zIndex: 99, borderBottomWidth: StyleSheet.hairlineWidth, borderColor: 'lightgray' }}
       />
       <StatusBar style={chroma.contrast(colorScheme.primary, "white") > 4.5 ? "light" : "dark"} translucent />
+      <ScrollView contentContainerStyle={{  padding: 16, paddingTop: insets.top + 56 + 16 }} scrollEventThrottle={16} onScroll={(e) => {
+        // setX(e.nativeEvent.contentOffset.y < 50)
+      }}>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto commodi consectetur
+          consequatur, cum deserunt dolor dolore dolorem ea eaque eum, excepturi fugiat id impedit in ipsam ipsum iste
+          iure laboriosam minima minus molestias nam natus necessitatibus nobis nulla numquam porro quae ratione rem
+          repellendus sed sunt tempora ullam veritatis voluptate voluptatibus. Animi aperiam architecto asperiores
+          assumenda at corporis culpa delectus, dolorem doloremque dolores eaque earum eos et illo in inventore maxime
+          minima mollitia necessitatibus obcaecati officiis omnis possimus provident quaerat, qui reiciendis repudiandae
+          sapiente sed sit soluta ut veniam veritatis vitae. Adipisci, aliquid blanditiis cumque delectus dolore
+          dolorum, ea exercitationem id in incidunt iste libero quos recusandae saepe sequi temporibus vitae voluptas
+          voluptatem? Adipisci atque autem commodi consectetur corporis culpa delectus, dignissimos dolor dolorem
+          expedita explicabo fugit hic illo iure nam, necessitatibus nobis nostrum omnis placeat porro quam quidem quod
+          reiciendis tempora voluptatum? Nostrum, voluptatem, voluptatibus! Alias aliquam assumenda eius ipsa. Alias
+          deserunt dicta doloribus non numquam officiis suscipit, ut. Atque consequatur distinctio dolorum eos eveniet
+          exercitationem, facere hic itaque iusto magni minus nobis non odit quaerat quibusdam, quos sapiente sed vel
+          velit vero. Facilis maxime quibusdam velit! Aut consectetur excepturi impedit, ipsa itaque laborum porro rem
+          repellat repudiandae sint! Accusamus architecto assumenda deserunt ducimus eaque est eveniet excepturi
+          expedita explicabo facere fuga incidunt minus natus nemo nobis obcaecati omnis pariatur perferendis placeat
+          quas quia quis, quod reiciendis rem sit totam velit voluptates! Aliquid dolores dolorum ducimus laboriosam
+          mollitia praesentium quo reprehenderit? A assumenda autem blanditiis cupiditate facere fuga ipsa itaque iure
+          laboriosam nam, officiis quae quidem repellendus totam ut. Debitis distinctio impedit necessitatibus quae
+          quisquam reprehenderit sapiente! Laborum molestias odio officiis pariatur quis repellendus velit? Accusantium
+          nemo non omnis quos recusandae? Ab accusantium aliquam animi at aut commodi dignissimos ducimus eos est eum
+          excepturi facere in modi, molestiae molestias necessitatibus, pariatur possimus ratione, sed temporibus. A
+          accusantium alias asperiores delectus dignissimos dolorem eaque error eveniet id impedit iste labore minus
+          nobis numquam omnis, quaerat quasi reiciendis reprehenderit sunt ullam velit voluptates.
+        </Text>
+      </ScrollView>
     </View>
   );
 };
