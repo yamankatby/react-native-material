920e11cf0b596166eb11425c1855ae4208d448d8
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-27 16:34:54 +0300

    [docs] Update Getting Started

---
+++ docs/docs/getting-started.md
@@ -0,0 +1,73 @@
+---
+sidebar_position: 1
+---
+
+# Getting started
+
+Get started with **React Native Material** with no time.
+
+## Installation
+
+Open a Terminal in your project's folder and run:
+
+```bash npm2yarn
+npm install @react-native-material/core
+```
+
+## Quick start
+
+Here's a quick example to get you started, **it's literally all you need**:
+
+```js
+import React from "react";
+import { Button } from "@react-native-material/core";
+
+export default function App() {
+  return <Button title="Button" onPress={() => alert("hi!")}/>;
+}
+```
+
+Yes, this really is all you need to get started, as you can see in this live and interactive demo:
+
+```js
+
+```
+
+## Community help (free)
+
+The community is your first stop for questions and advice about the framework. Welcome to the community!
+
+### StackOverflow
+
+For crowdsourced answers from expert **React Native Material** developers in our community. StackOverflow is also
+frequented, from time to time, by the maintainers of **React Native Material**.
+
+[Post a question](https://stackoverflow.com/questions/tagged/react-native-material)
+
+### GitHub
+
+**React Native Material** uses GitHub issues as a bug and feature request tracker. If you think you have found a bug, or
+have a new feature idea, please start by making sure it hasn't already
+been [reported or fixed](https://github.com/yamankatby/react-native-material/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aclosed)
+. You can search through existing issues and pull requests to see if someone has reported one similar to yours.
+
+[Open an issue](https://github.com/yamankatby/react-native-material/issues/new/choose)
+
+### Blog
+
+Stay up to date with the development of the library by following the blog.
+
+[Read the latest posts](/blog)
+
+## Contributing
+
+Please take a look at [Kanban](https://github.com/yamankatby/react-native-material/projects/1) where we have a roadmap
+for **React Native Material** community. Also, we have a list
+of [good first issues](https://github.com/yamankatby/react-native-material/labels/good%20first%20issue) that contain
+bugs that have a relatively limited scope. This is a great place to get started, gain experience, and get familiar with
+our contribution process.
+
+## License
+
+This library is licensed under the MIT License - see
+the [LICENSE](https://github.com/yamankatby/react-native-material/blob/main/LICENSE) file for details.
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,15 +4,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'React Native Material',
+  tagline: 'Modular and customizable Material Design UI components for React Native',
+  url: 'https://react-native-material.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yamankatby', // Usually your GitHub org/user name.
+  projectName: 'react-native-material', // Usually your repo name.
 
   presets: [
     [
@@ -22,93 +22,96 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/yamankatby/react-native-material/edit/main/docs/',
+          remarkPlugins: [
+            [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]
+          ]
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+            'https://github.com/yamankatby/react-native-material/edit/main/docs/blog/'
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'React Native Material',
+        // logo: {
+        //   alt: 'My Site Logo',
+        //   src: 'img/logo.svg'
+        // },
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'getting-started',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs'
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/yamankatby/react-native-material',
             label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            position: 'right'
+          }
+        ]
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+      // footer: {
+      //   style: 'dark',
+      //   links: [
+      //     {
+      //       title: 'Docs',
+      //       items: [
+      //         {
+      //           label: 'Tutorial',
+      //           to: '/docs/intro'
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       title: 'Community',
+      //       items: [
+      //         {
+      //           label: 'Stack Overflow',
+      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+      //         },
+      //         {
+      //           label: 'Discord',
+      //           href: 'https://discordapp.com/invite/docusaurus'
+      //         },
+      //         {
+      //           label: 'Twitter',
+      //           href: 'https://twitter.com/docusaurus'
+      //         }
+      //       ]
+      //     },
+      //     {
+      //       title: 'More',
+      //       items: [
+      //         {
+      //           label: 'Blog',
+      //           to: '/blog'
+      //         },
+      //         {
+      //           label: 'GitHub',
+      //           href: 'https://github.com/facebook/docusaurus'
+      //         }
+      //       ]
+      //     }
+      //   ],
+      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+      // },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        darkTheme: darkCodeTheme
+      }
+    })
 });
