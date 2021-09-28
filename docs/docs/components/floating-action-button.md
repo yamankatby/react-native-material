a4447a35fee2cd814b69cf32245b23adc63840ba
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-28 17:38:37 +0300

    [docs] Update docs

--- docs/docs/components/floating-action-button.md
+++ docs/docs/components/floating-action-button.md
@@ -2,6 +2,11 @@
 
 A floating action button (FAB) represents the primary action of a screen.
 
+<iframe src="http://localhost:19006/components/fab?section=overview"
+width="100%"
+height="300"
+style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
+
 A floating action button (FAB) performs the primary, or most common, action on a screen. It appears in front of all
 screen content, typically as a circular shape with an icon in its center. FABs come in three types: regular, mini, and
 extended.
@@ -11,83 +16,36 @@ Only use a FAB if it is the most suitable way to present a screen's primary acti
 [`💬 Feedback`](https://github.com/yamankatby/react-native-material/labels/component%3A%20Fab)
 [`🎨 Material Design`](https://material.io/components/buttons-floating-action-button)
 
-## Basic FAB
+## Variants
 
-<iframe src="http://localhost:19006/components/fab?section=basic"
+<iframe src="http://localhost:19006/components/fab?section=standard"
 width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-```js
-import { FAB } from "@react-native-material/core";
-import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
-
-<FAB icon={props => <Icon name="plus" {...props} />}/>
-<FAB icon={props => <Icon name="pencil" {...props} />} color="primary"/>
-<FAB
-  variant="extended"
-  icon={props => <Icon name="navigation" {...props} />}
-  label="Navigate"
-  color="primary"
-/>
-```
-
-## Size
-
-<iframe src="http://localhost:19006/components/fab?section=size"
+<iframe src="http://localhost:19006/components/fab?section=extended"
 width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-```js
-import { FAB } from "@react-native-material/core";
-import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+## Sizing
 
-<FAB icon={props => <Icon name="plus" {...props} />} color="primary" size="regular"/>
-<FAB icon={props => <Icon name="plus" {...props} />} color="primary" size="mini"/>
-```
-
-<iframe src="http://localhost:19006/components/fab?section=size-extended"
+<iframe src="http://localhost:19006/components/fab?section=standard-size"
 width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-```js
-import { FAB } from "@react-native-material/core";
-import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+<iframe src="http://localhost:19006/components/fab?section=extended-size"
+width="100%"
+height="300"
+style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-<FAB
-  variant="extended"
-  label="Button"
-  icon={props => <Icon name="plus" {...props} />}
-  color="primary"
-  size="regular"
-/>
-<FAB
-  variant="extended"
-  label="Button"
-  icon={props => <Icon name="plus" {...props} />}
-  color="primary"
-  size="mini"
-/>
-```
-
-## Color
+## Coloring
 
 <iframe src="http://localhost:19006/components/fab?section=color"
 width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
-
-```js
-import { FAB } from "@react-native-material/core";
-import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
-
-<FAB icon={props => <Icon name="lock" {...props} />}/>
-<FAB icon={props => <Icon name="lock" {...props} />} tintColor="red"/>
-<FAB icon={props => <Icon name="lock" {...props} />} color="onPrimary"/>
-<FAB icon={props => <Icon name="lock" {...props} />} color="#FDAC42"/>
-```
 
 ## Loading
 
@@ -96,12 +54,12 @@ width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-<iframe src="http://localhost:19006/components/fab?section=loading-extended"
+<iframe src="http://localhost:19006/components/fab?section=custom-loading"
 width="100%"
 height="300"
 style={{ marginTop: 18, marginBottom: 18, border: "1px solid rgba(0,0,0,.12)", borderRadius: 8 }} />
 
-## Visible
+## Visibility
 
 <iframe src="http://localhost:19006/components/fab?section=visible"
 width="100%"
--- example/screens/fab/FAB.tsx
+++ example/screens/fab/FAB.tsx
@@ -1,251 +1,252 @@
 import React, { useState } from "react";
-import { Platform, Switch, Text, View } from "react-native";
-import { FAB as FABComponent } from "@react-native-material/core";
+import { ScrollView, Switch, Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { Divider, FAB, Typography, useTheme } from "@react-native-material/core";
 import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
-import { useRoute } from '@react-navigation/native'
 
-export default function FAB() {
-  const { params } = useRoute()
-  const [isLoading, setIsLoading] = useState(true)
-  const [isVisible, setIsVisible] = useState(true)
+interface SectionProps {
+  name: string;
+  title: string;
+}
 
+const Section: React.FC<SectionProps> = ({ name, title, children }) => {
+  const params = useRoute().params as any;
+
+  if (params?.section && params?.section !== name) return null;
   return (
-    <View style={{ flex: 1 }}>
-      {(params?.section === undefined || params?.section === 'basic') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <FABComponent
-          icon={props => <Icon name="plus" {...props} />}
-          onPress={() => undefined}
-        />
-        <FABComponent
-          icon={props => <Icon name="pencil" {...props} />}
-          onPress={() => undefined}
-          color="primary"
-          style={{ marginStart: 16 }}
-        />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="navigation" {...props} />}
-          label="Navigate"
-          color="primary"
-          onPress={() => undefined}
-          style={{ marginStart: 16 }}
-        />
-      </View>
-      }
+    <View
+      style={[
+        { paddingStart: 16, paddingVertical: 16 },
+        params?.section === name && { flex: 1, justifyContent: "center", alignItems: "center" },
+      ]}
+    >
+      {params?.section !== name && (
+        <>
+          <Typography variant="h6">{title}</Typography>
+          <Divider style={{ marginVertical: 8 }} />
+        </>
+      )}
 
-      {(params?.section === undefined || params?.section === 'size') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <FABComponent
-          icon={props => <Icon name="plus" {...props} />}
-          onPress={() => undefined}
-          color="primary"
-        />
-        <FABComponent
-          icon={props => <Icon name="plus" {...props} />}
-          style={{ marginStart: 16 }}
-          onPress={() => undefined}
-          size="mini"
-          color="primary"
-        />
-      </View>
-      }
-
-      {(params?.section === undefined || params?.section === 'size-extended') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="plus" {...props} />}
-          label="button"
-          onPress={() => undefined}
-          color="primary"
-        />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="plus" {...props} />}
-          style={{ marginStart: 16 }}
-          onPress={() => undefined}
-          label="button"
-          size="mini"
-          color="primary"
-        />
-      </View>
-      }
-
-      {(params?.section === undefined || params?.section === 'color') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <FABComponent
-          icon={props => <Icon name="lock" {...props} />}
-          onPress={() => undefined}
-        />
-        <FABComponent
-          icon={props => <Icon name="lock" la {...props} />}
-          tintColor="red"
-          style={{ marginStart: 16 }}
-          onPress={() => undefined}
-        />
-        <FABComponent
-          icon={props => <Icon name="lock" {...props} />}
-          color="onPrimary"
-          style={{ marginStart: 16 }}
-          onPress={() => undefined}
-        />
-        <FABComponent
-          color="#fdac42"
-          icon={props => <Icon name="lock" {...props} />}
-          style={{ marginStart: 16 }}
-          onPress={() => undefined}
-        />
-      </View>
-      }
-
-      {(params?.section === undefined || params?.section === 'loading') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <Switch value={isLoading} onValueChange={val => setIsLoading(val)} />
-        <FABComponent
-          icon={props => <Icon name="lock" {...props} />}
-          loading={isLoading}
-          onPress={() => undefined}
-        />
-        <FABComponent
-          icon={props => <Icon name="lock" {...props} />}
-          loading={isLoading}
-          onPress={() => undefined}
-          size="mini"
-          style={{ marginStart: 16 }}
-        />
-      </View>
-      }
-
-      {(params?.section === undefined || params?.section === 'loading-extended') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <Switch value={isLoading} onValueChange={val => setIsLoading(val)} />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="plus" {...props} />}
-          label="button"
-          onPress={() => undefined}
-          loading={isLoading}
-          style={{ marginStart: 16 }}
-          color="primary"
-        />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="plus" {...props} />}
-          label="button"
-          loadingIndicator="Loading..."
-          loadingIndicatorPosition="overlay"
-          onPress={() => undefined}
-          loading={isLoading}
-          style={{ marginStart: 16 }}
-          color="primary"
-        />
-        <FABComponent
-          variant="extended"
-          label="button"
-          size="mini"
-          color="onError"
-          loadingIndicator={props => <Text style={{ color: props.color }}>⏰</Text>}
-          loadingIndicatorPosition="icon"
-          onPress={() => undefined}
-          loading={isLoading}
-          style={{ marginStart: 16 }}
-        />
-      </View>
-      }
-
-      {(params?.section === undefined || params?.section === 'visible') &&
-      <View style={{
-        flexDirection: 'row', ...Platform.select({
-          web: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-        }),
-      }}>
-        <Switch value={isVisible} onValueChange={val => setIsVisible(val)} />
-        <FABComponent
-          icon={props => <Icon name="plus" {...props} />}
-          onPress={() => undefined}
-          visible={isVisible}
-        />
-        <FABComponent
-          icon={props => <Icon name="plus" {...props} />}
-          onPress={() => undefined}
-          style={{ marginStart: 16 }}
-          size="mini"
-          visible={isVisible}
-        />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="navigation" {...props} />}
-          label="Navigate"
-          color="primary"
-          onPress={() => undefined}
-          style={{ marginStart: 16 }}
-          visible={isVisible}
-        />
-        <FABComponent
-          variant="extended"
-          icon={props => <Icon name="navigation" {...props} />}
-          label="Navigate"
-          color="primary"
-          size="mini"
-          onPress={() => undefined}
-          style={{ marginStart: 16 }}
-          visible={isVisible}
-        />
-      </View>
-      }
+      <View style={{ flexDirection: "row", alignItems: "center" }}>{children}</View>
     </View>
   );
-}
+};
+
+const FABExample = () => {
+  const theme = useTheme();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const separator = <View style={{ width: 16, height: 16 }} />;
+
+  return (
+    <ScrollView style={{ flex: 1 }} contentContainerStyle={{flex: 1}}>
+      <Section name="overview" title="Overview">
+        <FAB icon={props => <Icon name="plus" {...props} />} onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="pencil" {...props} />} loading color="primary" onPress={() => undefined} />
+        {separator}
+        <FAB
+          icon={props => <Icon name="navigation" {...props} />}
+          variant="extended"
+          label="Navigate"
+          color="primary"
+          onPress={() => undefined}
+        />
+      </Section>
+
+      <Section name="standard" title="Standard FAB">
+        <FAB icon={props => <Icon name="star" {...props} />} color="primary" onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="star" {...props} />} color="primary" loading onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="star" {...props} />} size="mini" onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="star" {...props} />} size="mini" loading onPress={() => undefined} />
+      </Section>
+
+      <Section name="extended" title="Extended FAB">
+        <FAB variant="extended" label="Button" color="primary" onPress={() => undefined} />
+        {separator}
+        <FAB
+          icon={props => <Icon name="pencil" {...props} />}
+          variant="extended"
+          label="Button"
+          color="primary"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          icon={props => <Icon name="navigation" {...props} />}
+          variant="extended"
+          label="Button"
+          color="primary"
+          onPress={() => undefined}
+          size="mini"
+        />
+        {separator}
+        <FAB
+          icon={props => <Icon name="navigation" {...props} />}
+          variant="extended"
+          label="Button"
+          color="primary"
+          onPress={() => undefined}
+          size="mini"
+          loading
+        />
+      </Section>
+
+      <Section name="standard-size" title="Standard FAB Size">
+        <FAB icon={props => <Icon name="account" {...props} />} onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="account" {...props} />} size="mini" onPress={() => undefined} />
+      </Section>
+
+      <Section name="extended-size" title="Extended FAB Size">
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="heart" {...props} />}
+          label="Button"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="heart" {...props} />}
+          label="Button"
+          size="mini"
+          onPress={() => undefined}
+        />
+      </Section>
+
+      <Section name="color" title="Coloring">
+        <FAB icon={props => <Icon name="lock" {...props} />} onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="lock" {...props} />} tintColor="red" onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="lock" {...props} />} color="onPrimary" onPress={() => undefined} />
+        {separator}
+        <FAB icon={props => <Icon name="lock" {...props} />} color="#FDCAE4" onPress={() => undefined} />
+      </Section>
+
+      <Section name="loading" title="Loading">
+        <Switch tintColor={theme.colorScheme.secondary} value={isLoading} onValueChange={val => setIsLoading(val)} />
+        {separator}
+        <FAB
+          icon={props => <Icon name="plus" {...props} />}
+          loading={isLoading}
+          color="primary"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="plus" {...props} />}
+          label="Button"
+          loading={isLoading}
+          color="primary"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="plus" {...props} />}
+          label="Button"
+          loading={isLoading}
+          loadingIndicatorPosition="overlay"
+          color="primary"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          icon={props => <Icon name="plus" {...props} />}
+          size="mini"
+          loading={isLoading}
+          color="primary"
+          onPress={() => undefined}
+        />
+      </Section>
+
+      <Section name="custom-loading" title="Custom Loading">
+        <Switch tintColor={theme.colorScheme.error} value={isLoading} onValueChange={val => setIsLoading(val)} />
+        {separator}
+        <FAB
+          icon={props => <Icon name="plus" {...props} />}
+          loading={isLoading}
+          color="error"
+          loadingIndicator="⏳"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="plus" {...props} />}
+          label="Button"
+          loading={isLoading}
+          loadingIndicator={props => (
+            <Text
+              style={{
+                backgroundColor: props.color,
+                color: "white",
+                borderRadius: 50,
+                width: 24,
+                height: 24,
+                textAlign: "center",
+              }}
+            >
+              ...
+            </Text>
+          )}
+          color="onError"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          variant="extended"
+          icon={props => <Icon name="plus" {...props} />}
+          label="Button"
+          loading={isLoading}
+          loadingIndicatorPosition="overlay"
+          loadingIndicator="Loading..."
+          color="onError"
+          onPress={() => undefined}
+        />
+        {separator}
+        <FAB
+          icon={props => <Icon name="plus" {...props} />}
+          size="mini"
+          loading={isLoading}
+          loadingIndicator="⏰"
+          color="onError"
+          onPress={() => undefined}
+        />
+      </Section>
+
+      <Section name="visible" title="Visibility">
+        <Switch value={isVisible} onValueChange={val => setIsVisible(val)} />
+        {separator}
+        <FAB icon={props => <Icon name="plus" {...props} />} onPress={() => undefined} visible={isVisible} />
+        {separator}
+        <FAB
+          icon={props => <Icon name="plus" {...props} />}
+          onPress={() => undefined}
+          size="mini"
+          visible={isVisible}
+        />
+        {separator}
+        <FAB
+          icon={props => <Icon name="navigation" {...props} />}
+          variant="extended"
+          label="Navigate"
+          color="primary"
+          onPress={() => undefined}
+          visible={isVisible}
+        />
+      </Section>
+    </ScrollView>
+  );
+};
+
+export default FABExample;
