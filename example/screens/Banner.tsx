a1d673ff68f59d331e7579da34b6fb21382b964f
Yaman KATBY <m.yaman.katby@gmail.com>
2021-09-30 11:12:16 +0300

    [docs] Update Banner doc

--- example/screens/Banner.tsx
+++ example/screens/Banner.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useRoute } from "@react-navigation/native";
 import { ScrollView } from "react-native";
 import { Section } from "./FAB";
+import { Avatar, Banner, Button } from "@react-native-material/core";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
 
 const BannerExample = () => {
   const params = useRoute().params as any;
@@ -11,7 +13,16 @@ const BannerExample = () => {
       contentContainerStyle={params?.section && { flex: 1 }}
     >
       <Section name="overview" title="Overview">
-
+        <Banner
+          style={{ maxWidth: 360 }}
+          text="There was a problem processing a transaction on your credit card."
+          buttons={[
+            <Button variant="text" compact title="Fix it" onPress={() => undefined} />,
+            <Button variant="text" compact title="Learn More" onPress={() => undefined} />,
+          ]}
+          illustration={props => <Avatar {...props} color="primary"
+                                         icon={props => <Icon name="wifi-off" {...props} />} />}
+        />
       </Section>
     </ScrollView>
   )
