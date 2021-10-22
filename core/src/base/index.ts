ad73baa1254a94f3e39882ff8d1e4298a8c649e4
Yaman KATBY <m.yaman.katby@gmail.com>
2021-10-22 11:45:13 +0300

    [core] Refactor theme files

--- core/src/base/index.ts
+++ core/src/base/index.ts
@@ -1,6 +1,2 @@
-export * from "./color-scheme";
 export * from "./context";
-export * from './elevation-scheme'
-export * from "./shape-scheme";
 export * from "./theme";
-export * from "./typography-scheme";
--- core/src/base/theme.ts
+++ core/src/base/theme.ts
@@ -1,8 +1,61 @@
-import { Platform } from "react-native";
-import { ColorScheme } from "./color-scheme";
-import { ShapeScheme } from "./shape-scheme";
-import { TypographyScheme } from "./typography-scheme";
-import { ElevationScheme } from "./elevation-scheme";
+import { ImageStyle, Platform, TextStyle, ViewStyle } from "react-native";
+
+export type ColorName =
+  | "primary"
+  | "primaryVariant"
+  | "secondary"
+  | "secondaryVariant"
+  | "background"
+  | "surface"
+  | "error"
+  | "onPrimary"
+  | "onSecondary"
+  | "onBackground"
+  | "onSurface"
+  | "onError";
+
+export type ColorScheme = Record<ColorName, string>;
+
+export type ShapeCategory = "small" | "medium" | "large";
+
+export type ShapeFamily = "rounded" | "cut";
+
+export interface ShapeBorderRadius {
+  topStart: number;
+  topEnd: number;
+  bottomStart: number;
+  bottomEnd: number;
+}
+
+export interface ShapeValues {
+  family: ShapeFamily;
+  borderRadius: ShapeBorderRadius;
+}
+
+export type ShapeScheme = Record<ShapeCategory, ShapeValues>;
+
+export type TypographyVariant =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "subtitle1"
+  | "subtitle2"
+  | "body1"
+  | "body2"
+  | "button"
+  | "caption"
+  | "overline";
+
+export type TypographyScheme = Record<TypographyVariant, TextStyle>;
+
+export type Elevation = 0 | 1 | 2 | 3 | 4 | 6 | 8 | 12 | 16 | 24;
+
+export type ElevationStyle = ViewStyle | TextStyle | ImageStyle;
+
+export type ElevationScheme = Record<Elevation, ElevationStyle>;
 
 export type Mode = "light" | "dark";
 
