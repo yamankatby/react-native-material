3578279a499a31c65c74f8c4af7dc335cbf30676
Yaman KATBY <m.yaman.katby@gmail.com>
2021-11-17 16:13:28 +0300

    [docs] Update examples to use v1.2.1

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -63,7 +63,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             type: 'dropdown',
             label: 'v1.2.0',
             items: [
-              { label: 'v1.2.0', to: '/' },
+              { label: 'v1.2.1', to: '/' },
               {
                 label: 'v1.1.0',
                 href: 'https://react-native-material-38lpom7xc-yamankatby.vercel.app/docs/getting-started'
