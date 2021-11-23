ef80880eca6dc4370a0e51578a3df2d63fef4677
Yaman KATBY <m.yaman.katby@gmail.com>
2021-11-23 14:39:52 +0300

    [core] Update IconButton component to use the new Pressable

--- core/src/IconButton.tsx
+++ core/src/IconButton.tsx
@@ -1,34 +1,75 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, ViewProps } from "react-native";
 import { PaletteColor, usePalette } from "./base";
 import Pressable, { PressableProps } from "./Pressable";
 
-export interface IconButtonProps extends PressableProps {
+export interface IconButtonProps extends Omit<ViewProps, "hitSlop">, Omit<PressableProps, "style" | "children"> {
   icon?: React.ReactElement;
 
   color?: PaletteColor;
+
+  contentContainerStyle?: PressableProps["style"];
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
     width: 48,
     height: 48,
     borderRadius: 48 / 2,
+    overflow: "hidden",
+  },
+  contentContainer: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
-const IconButton: React.FC<IconButtonProps> = ({ icon, color = "onBackground", style, ...rest }) => {
+const IconButton: React.FC<IconButtonProps> = ({
+  icon,
+  color = "onBackground",
+  style,
+  contentContainerStyle,
+  effect,
+  effectColor,
+  onPress,
+  onPressIn,
+  onPressOut,
+  onLongPress,
+  onBlur,
+  onFocus,
+  delayLongPress,
+  disabled,
+  hitSlop,
+  pressRetentionOffset,
+  android_disableSound,
+  android_ripple,
+  testOnly_pressed,
+  ...rest
+}) => {
   const palette = usePalette(color);
   return (
-    <Pressable
-      style={[styles.container, style]}
-      effectColor={palette.color}
-      {...rest}
-    >
-      {icon}
-    </Pressable>
+    <View style={[styles.container, style]} {...rest}>
+      <Pressable
+        style={[StyleSheet.absoluteFill, styles.contentContainer, contentContainerStyle]}
+        effect={effect}
+        effectColor={effectColor ?? palette.color}
+        onPress={onPress}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        onLongPress={onLongPress}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        delayLongPress={delayLongPress}
+        disabled={disabled}
+        hitSlop={hitSlop}
+        pressRetentionOffset={pressRetentionOffset}
+        android_disableSound={android_disableSound}
+        android_ripple={android_ripple}
+        testOnly_pressed={testOnly_pressed}
+      >
+        {icon}
+      </Pressable>
+    </View>
   );
 };
 
