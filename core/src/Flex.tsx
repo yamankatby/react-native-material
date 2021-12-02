688e099dccb8b096b16228ee6dd6604b989330c8
Yaman KATBY <m.yaman.katby@gmail.com>
2021-12-02 19:05:41 +0300

    [core] Work on the Stack component

--- core/src/Flex.tsx
+++ core/src/Flex.tsx
@@ -2,24 +2,73 @@ import React from "react";
 import { FlexStyle, View, ViewProps } from "react-native";
 
 export interface FlexProps extends ViewProps {
+  /**
+   * Define how your items are going to “fill” over the available space along your main axis.
+   */
   flex?: boolean | FlexStyle["flex"];
 
+  /**
+   * Shorthand for `flexDirection` style property.
+   * @type FlexStyle["flexDirection"]
+   *
+   * @default "column"
+   */
   direction?: FlexStyle["flexDirection"];
 
+  /**
+   * Shorthand for `justifyContent` style property.
+   * @type FlexStyle["justifyContent"]
+   *
+   * @default "flex-start"
+   */
   justify?: FlexStyle["justifyContent"];
 
+  /**
+   * Shorthand for `alignItems` style property.
+   * @type FlexStyle["alignItems"]
+   *
+   * @default "stretch"
+   */
   align?: FlexStyle["alignItems"];
 
+  /**
+   * Shorthand for `alignSelf` style property.
+   * @type FlexStyle["alignSelf"]
+   */
   self?: FlexStyle["alignSelf"];
 
+  /**
+   * Shorthand for `alignContent` style property.
+   * @type FlexStyle["alignContent"]
+   *
+   * @default "flex-start"
+   */
   content?: FlexStyle["alignContent"];
 
+  /**
+   * Shorthand for `flexWrap` style property.
+   * @type FlexStyle["flexWrap"]
+   */
   wrap?: FlexStyle["flexWrap"];
 
+  /**
+   * Shorthand for `flexBasis` style property.
+   * @type FlexStyle["flexBasis"]
+   */
   basis?: FlexStyle["flexBasis"];
 
+  /**
+   * Shorthand for `flexGrow` style property.
+   * @type FlexStyle["flexGrow"]
+   */
   grow?: FlexStyle["flexGrow"];
 
+  /**
+   * Shorthand for `flexShrink` style property.
+   * @type FlexStyle["flexShrink"]
+   *
+   * @default 0
+   */
   shrink?: FlexStyle["flexShrink"];
 }
 
@@ -35,7 +84,7 @@ const Flex: React.FC<FlexProps> = ({
   grow,
   shrink,
   style,
-  ...props
+  ...rest
 }) => (
   <View
     style={[
@@ -53,7 +102,7 @@ const Flex: React.FC<FlexProps> = ({
       },
       style,
     ]}
-    {...props}
+    {...rest}
   />
 );
 
--- core/src/Stack.tsx
+++ core/src/Stack.tsx
@@ -1,111 +1,54 @@
 import React from "react";
-import { View, ViewProps, ViewStyle } from "react-native";
+import Flex, { FlexProps } from "./Flex";
+import { View } from "react-native";
 
-export interface StackProps extends ViewProps {
-  /**
-   * The direction of the stack.
-   * - `column` - Vertical stack.
-   * - `row` - Horizontal stack.
-   * - `row-reverse` - Horizontal stack with reverse order of children.
-   * - `column-reverse` - Vertical stack with reverse order of children.
-   *
-   * @default "column"
-   */
-  direction?: ViewStyle["flexDirection"];
-
-  /**
-   * The alignment of the stack items along the stack's main axis.
-   *
-   * @default "flex-start"
-   */
-  justify?: ViewStyle["justifyContent"];
-
-  /**
-   * The alignment of the stack items along the stack's cross axis.
-   *
-   * @default "stretch"
-   */
-  align?: ViewStyle["alignItems"];
-
-  /**
-   * Whether the stack items forced in a single line or can be flowed into multiple lines.
-   *
-   * @default "nowrap"
-   */
-  wrap?: ViewStyle["flexWrap"];
-
+export interface StackProps extends FlexProps {
   /**
    * The spacing between the stack items.
-   *
-   * @default 0
    */
   spacing?: number;
 
   /**
    * A React.Node to render between each stack item.
-   *
-   * @default 0
    */
   divider?: React.ReactNode;
 }
 
-export function getValidChildren(children: React.ReactNode) {
-  return React.Children.toArray(children).filter(child => React.isValidElement(child)) as React.ReactElement[];
-}
-
-const Stack: React.FC<StackProps> = ({
-  direction = "column",
-  justify = "flex-start",
-  align = "stretch",
-  wrap = "nowrap",
-  spacing = 0,
-  divider,
-  style,
-  children,
-  ...rest
-}) => {
+const Stack: React.FC<StackProps> = ({ spacing, divider, children, ...rest }) => {
   const validChildren = getValidChildren(children);
 
-  const clones = validChildren.map((child, index) => {
-    const key = typeof child.key !== "undefined" ? child.key : index;
-    const isLast = index + 1 === validChildren.length;
+  const clones =
+    spacing === undefined && divider === undefined
+      ? children
+      : validChildren.map((child, index) => {
+        const key = typeof child.key !== "undefined" ? child.key : index;
+        const isLast = index === validChildren.length - 1;
 
-    const dividerElement = divider && !isLast ? divider : null;
+        const horizontal = rest.direction === "row" || rest.direction === "row-reverse";
+        const spacingValue = spacing ? spacing * 4 : undefined;
 
-    const spacingElement =
-      spacing && !isLast ? <View style={{ [direction === "column" ? "height" : "width"]: spacing * 4 }} /> : null;
+        return (
+          <React.Fragment key={key}>
+            {child}
+            {!isLast && (
+              <View
+                style={[
+                  { [horizontal ? "marginStart" : "marginTop"]: spacingValue },
+                  !!divider && { [horizontal ? "marginEnd" : "marginBottom"]: spacingValue },
+                ]}
+              >
+                {divider}
+              </View>
+            )}
+          </React.Fragment>
+        );
+      });
 
-    return (
-      <React.Fragment key={key}>
-        {child}
-        {dividerElement && (
-          <>
-            {spacingElement}
-            {dividerElement}
-          </>
-        )}
-        {spacingElement}
-      </React.Fragment>
-    );
-  });
-
-  return (
-    <View
-      style={[
-        {
-          flexDirection: direction,
-          justifyContent: justify,
-          alignItems: align,
-          flexWrap: wrap,
-        },
-        style,
-      ]}
-      {...rest}
-    >
-      {clones}
-    </View>
-  );
+  return <Flex {...rest}>{clones}</Flex>;
 };
+
+const getValidChildren = (children: React.ReactNode) =>
+  React.Children.toArray(children).filter(child => React.isValidElement(child)) as React.ReactElement[];
 
 export type HStackProps = Omit<StackProps, "direction">;
 
