348735ae79ea78de8bf8bad7a156fcbedb5656d3
Yaman KATBY <m.yaman.katby@gmail.com>
2021-12-25 17:28:56 +0300

    [core] Work on the Surface component

--- core/src/Surface.tsx
+++ core/src/Surface.tsx
@@ -1,27 +1,35 @@
 import React from "react";
 import { Animated, StyleProp, ViewProps, ViewStyle } from "react-native";
-import { Elevation, ShapeCategory, useTheme } from "./base/ThemeContext";
-import { useSurfaceColorValue } from "./hooks/use-surface-color";
+import { Elevation, ShapeCategory } from "./base/ThemeContext";
+import { useStyles } from "./hooks/use-styles";
 
 export interface SurfaceProps extends Omit<ViewProps, "style"> {
+  /**
+   * The relative size of the surface.
+   */
   category?: ShapeCategory;
 
+  /**
+   * The elevation of the surface.
+   */
   elevation?: Elevation;
 
   style?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
 }
 
 const Surface: React.FC<SurfaceProps> = ({ category, elevation = 0, style, ...rest }) => {
-  const { shapes, elevations } = useTheme();
-
-  const backgroundColor = useSurfaceColorValue(elevation);
-
-  return (
-    <Animated.View
-      style={[elevations[elevation], { backgroundColor }, category && shapes[category], style]}
-      {...rest}
-    />
+  const styles = useStyles(
+    ({ elevations, shapes, surfaceColor }) => ({
+      surface: {
+        ...elevations[elevation],
+        ...(category ? shapes[category] : {}),
+        backgroundColor: surfaceColor(elevation).hex(),
+      },
+    }),
+    [category, elevation],
   );
+
+  return <Animated.View style={[styles.surface, style]} {...rest} />;
 };
 
 export default Surface;
--- core/src/hooks/use-styles.ts
+++ core/src/hooks/use-styles.ts
@@ -1,20 +1,36 @@
 import { DependencyList, useMemo } from "react";
 import { ImageStyle, TextStyle, ViewStyle } from "react-native";
+import chroma from "chroma-js";
 import { Theme, useTheme } from "../base/ThemeContext";
 import { useWindowSizeClass, WindowSizeClass } from "../base/WindowSizeClassContext";
 import { useWindowSize, WindowSize } from "./use-window-size";
 import { Spacing, useSpacing } from "../base/SpacingContext";
+import { useSurfaceScale } from "./use-surface-scale";
+import { useSurfaceColor } from "./use-surface-color";
 
 type NamedStyles<T> = { [P in keyof T]: ViewStyle | TextStyle | ImageStyle };
 
 export const useStyles = <T extends NamedStyles<T>>(
-  factory: (utils: Theme & { windowSizeClass: WindowSizeClass; windowSize: WindowSize; spacing: Spacing }) => T,
+  factory: (
+    utils: Theme & {
+      surfaceScale: chroma.Scale<chroma.Color>;
+      surfaceColor: chroma.Scale<chroma.Color>;
+      windowSizeClass: WindowSizeClass;
+      windowSize: WindowSize;
+      spacing: Spacing;
+    },
+  ) => T,
   deps?: DependencyList | undefined,
 ): T => {
   const theme = useTheme();
+  const surfaceScale = useSurfaceScale();
+  const surfaceColor = useSurfaceColor();
   const windowSizeClass = useWindowSizeClass();
   const windowSize = useWindowSize();
   const spacing = useSpacing();
 
-  return useMemo(() => factory({ ...theme, windowSizeClass, windowSize, spacing }), [theme, windowSizeClass, deps]);
+  return useMemo(
+    () => factory({ ...theme, surfaceScale, surfaceColor, windowSizeClass, windowSize, spacing }),
+    [theme, windowSizeClass, deps],
+  );
 };
